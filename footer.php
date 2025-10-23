07e35bc33e293fa970b0a8961b716feb7e89e26f
Nicolás Zamora <nzamora@uvaq.edu.mx>
2025-10-22 19:57:03 -0600

    Main Files
    Adding the main files

---
+++ footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        <p>&copy; 2025 My Website</p>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    
+    get_header();
+?>
+
+    <main>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+            <h2><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+
+        <?php endwhile; endif; ?>
+    </main>
+    
+
+<?php
+
+    get_footer();
+
+?>   
